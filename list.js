160f594ffcd2cf09d0aad7fbbec9029aeaf510f4
smartserveteam <smartserveteam@gmail.com>
2019-04-08 09:48:02 +0200

    trying to get categories

--- list.js
+++ list.js
@@ -4,16 +4,8 @@ import { success, failure } from "./libs/response-lib";
 export async function main(event, context) {
   const params = {
     TableName: process.env.tableName,
-    // 'KeyConditionExpression' defines the condition for the query
-    // - 'userId = :userId': only return items with matching 'userId'
-    //   partition key
-    // 'ExpressionAttributeValues' defines the value in the condition
-    // - ':userId': defines 'userId' to be Identity Pool identity id
-    //   of the authenticated user
-    KeyConditionExpression: "userId = :userId",
-    ExpressionAttributeValues: {
-      ":userId": event.requestContext.identity.cognitoIdentityId
-    }
+
+    // ANY PARAMS REQUIRED? STILL GETTING 500
   };
 
   try {
