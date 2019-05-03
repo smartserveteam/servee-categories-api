ebcf3fba80d3deb0b6f5077b9dab300ab345d70d
osboxes.org <osboxes@smartserve.home>
2019-05-03 14:56:50 +0200

    Add skills search

--- get.js
+++ get.js
@@ -2,22 +2,24 @@ import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
+  console.log("Event:", event);
   const params = {
-    TableName: process.env.tableName,
-    // 'Key' defines the partition key and sort key of the item to be retrieved
-    // - 'categoryId': path parameter
-    Key: {
-      categoryId: event.pathParameters.id
+    TableName: process.env.DYNAMODB_TABLE,
+    FilterExpression: "contains(skills, :s)",
+    ExpressionAttributeValues: {
+      ":s": event.pathParameters.id
     }
   };
 
   try {
-    const result = await dynamoDbLib.call("get", params);
-    if (result.Item) {
-      // Return the retrieved item
-      return success(result.Item);
+    console.log("Params:", params);
+    const result = await dynamoDbLib.call("scan", params);
+    console.log("Result:", result);
+    if (result.Items) {
+      // Return the retrieved items
+      return success(result.Items);
     } else {
-      return failure({ status: false, error: "Item not found." });
+      return failure({ status: false, error: "Items not found." });
     }
   } catch (e) {
     return failure({ status: false, error: e });
